c16abbd4a9f9fcad0a0b47842a216885c4948b87
Clervens Volcy <clervens.volcy@gmail.com>
2021-05-21 19:59:39 -0400

    Add TokenSale contract to sale tokens

--- contracts/CVolcyTokenSale.sol
+++ contracts/CVolcyTokenSale.sol
@@ -1,8 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-contract CVolcyTokenSale {
-    constructor() {
+import './CVolcyToken.sol';
 
+contract CVolcyTokenSale {
+    address admin;
+    CVolcyToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
+
+    constructor(CVolcyToken _tokenContract, uint256 _tokenPrice) {
+        admin = msg.sender;
+        tokenContract = _tokenContract;
+        tokenPrice = _tokenPrice;
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x);
     }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var CVolcyToken = artifacts.require('./CVolcyToken.sol');
 var CVolcyTokenSale = artifacts.require('./CVolcyTokenSale.sol');
 
-module.exports = function(deployer) {
-    deployer.deploy(CVolcyToken, 1000000 * 10 ** 5, 'CVolcyToken', 5, 'cvol');
-    deployer.deploy(CVolcyTokenSale);
+module.exports = async function(deployer) {
+    await deployer.deploy(CVolcyToken, 1000000 * 10 ** 5, 'CVolcyToken', 5, 'cvol');
+
+    const tokenPrice = 10000000000000; // in wei = 0.00001 eth
+    await deployer.deploy(CVolcyTokenSale, CVolcyToken.address, tokenPrice);
 }
