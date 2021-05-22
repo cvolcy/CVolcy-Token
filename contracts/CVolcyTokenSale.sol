c8527888f2f9f6d5bf59206421c45114915db685
Clervens Volcy <clervens.volcy@gmail.com>
2021-05-22 13:44:55 -0400

    check if theres enough tokens to transfer

--- contracts/CVolcyTokenSale.sol
+++ contracts/CVolcyTokenSale.sol
@@ -19,6 +19,8 @@ contract CVolcyTokenSale {
 
     function buyTokens(uint256 _numberOfTokens) public payable {
         require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
         tokensSold += _numberOfTokens;
 
         emit Sell(msg.sender, _numberOfTokens);
